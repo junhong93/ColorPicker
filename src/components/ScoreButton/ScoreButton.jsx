ea3733851f17765c9f69448c5fa424c752ca1b03
Jun Hong Park <junhong93@gmail.com>
2020-09-16 16:57:27 -0400

    practice React

---
+++ src/components/ScoreButton/ScoreButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ScoreButton.module.css';
+
+
+console.log(styles)
+
+const ScoreButton = (props) => (
+  <button className={`${styles.button} btn btn-default`}>
+    ✔
+  </button>
+);
+
+export default ScoreButton;
